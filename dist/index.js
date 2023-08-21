1d2d3e4ec3d29f99f29d051442037e148cf948e3
szy-kenn <johnkennethvelano@gmail.com>
2023-08-21 16:27:41 +0800

    made cell generation dynamic

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-function isLetter(str) {
-    let letterRegex = /^[a-zA-Z]$/; // check from start to end if it matches with any letters ranging from a-Z
-    return letterRegex.test(str) && str.length == 1;
-    // returns true if it matches with the regex AND if it is a single character
-}
-document.addEventListener("keydown", (event) => {
-    console.log(event.key);
-});
--- src/dist/main.js
+++ src/dist/main.js
@@ -1,35 +1,28 @@
 // global variables
-const WORD_LENGTH = 6;        // length of the word to be guessed
-const TRIES = 6;              // maximum number of guesses
-
+const WORD_LENGTH = 6; // length of the word to be guessed
+const TRIES = 6; // maximum number of guesses
 // main containers
-const gridContainer = document.querySelector<HTMLElement>(".wordle-grid-container") // container of all row cotainers containing letter boxes 
-
-function initialize(length: number, tries: number, container: Element) {
+const gridContainer = document.querySelector(".wordle-grid-container"); // container of all row cotainers containing letter boxes 
+function initialize(length, tries, container) {
     for (let i = 0; i < tries; i++) {
         // create a new row 
         const row = document.createElement("div");
         row.classList.add('row-container');
-
         // add the squares in the row
         for (let j = 0; j < length; j++) {
             const square = document.createElement("div");
-            square.classList.add("square"); 
+            square.classList.add("square");
             row.appendChild(square);
         }
         container.appendChild(row);
     }
 }
-
-function isLetter(str: string) {
+function isLetter(str) {
     let letterRegex = /^[a-zA-Z]$/; // check from start to end if it matches with any letters ranging from a-Z
-    return letterRegex.test(str) && str.length == 1; 
+    return letterRegex.test(str) && str.length == 1;
     // returns true if it matches with the regex AND if it is a single character
 }
-
 initialize(WORD_LENGTH, TRIES, gridContainer);
-
 document.addEventListener("keydown", (event) => {
     console.log(event.key);
-})
-
+});
