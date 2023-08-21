56c4586c90a9d16531ea49289157de3086120b96
szy-kenn <johnkennethvelano@gmail.com>
2023-08-21 18:43:11 +0800

    added pop, out , shake animation

--- src/dist/main.js
+++ src/dist/main.js
@@ -71,7 +71,9 @@ document.addEventListener("keydown", (event) => {
         if (isLetter(event.key) && (currentSquare <= WORD_LENGTH - 1)) {
             const currentCell = getCell(currentRow, currentSquare); // get current cell to fill 
             currentCell.firstElementChild.textContent = event.key; // change text content to the corresponding event key
+            currentCell.classList.add('popped');
             currentCell.classList.add('filled'); // change the border color to white (filled cell)
+            currentCell.classList.remove('out');
             // update the tracker variables
             currentSquare++;
         }
@@ -79,7 +81,9 @@ document.addEventListener("keydown", (event) => {
             if (currentSquare !== 0) {
                 const currentCell = getCell(currentRow, currentSquare - 1);
                 currentCell.firstElementChild.textContent = "";
+                currentCell.classList.add('out');
                 currentCell.classList.remove('filled');
+                currentCell.classList.remove('popped');
                 currentSquare--;
             }
         }
